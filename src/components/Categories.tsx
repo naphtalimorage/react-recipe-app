5ab5a6435d216f746765250aac62b57717677500
naphtali morage <naphtalimorage56@gmail.com>
2025-03-09 19:34:36 +0300

    fix spacing in Categories component for improved layout consistency

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -73,7 +73,7 @@ const Categories = () => {
             </div>
             <div className="mt-10">
                 {!loading ? (
-                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-3  gap-4">
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-3 gap-4">
                         {Array.from({length: 25}).map((_, index) => (
                             <SkeletonCard key={index}/>
                         ))}
