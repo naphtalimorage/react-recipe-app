e554120aa544622eb0ab8f310a933dd6dce2da6b
naphtali-morage <naphtalimorage56@gmail.com>
2025-03-21 12:03:53 +0300

    update Home component to use full screen height and enable vertical scrolling; improve layout responsiveness

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar.tsx"
 
 const Home = () => {
     return(
-        <div className="h-200vh bg-green-50 w-full p-8 space-y-4">
+        <div className="h-screen overflow-hidden overflow-y-auto bg-green-50 w-full p-8 space-y-4">
             <SearchBar/>
             <Categories/>
         </div>
